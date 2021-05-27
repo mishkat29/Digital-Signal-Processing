8d663d80d944ed01cdbb42345b6f84ef90fd5194
Mishkat Ahmad Neyazi <49838423+mishkat29@users.noreply.github.com>
2021-05-27 23:18:08 +0530

    Add files via upload

---
+++ IFFT_in-built.m
@@ -0,0 +1,7 @@
+% To compute IDFT of the signal given below using in-built  
+% function in MATLAB m= [0, 0, 4, 0, 0, 0, 4, 0]
+% Name- Mishkat Ahmad Neyazi 
+% Reg No.- 189402164 Sec- ECE-C Batch- C3
+m= [0, 0, 4, 0, 0, 0, 4, 0];
+x= ifft(m);
+plot(m,x)
